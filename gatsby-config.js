b51207222426ef0164523d387a88e8857e315672
Sahil Purav <sahil.purav@cactusglobal.com>
2021-07-05 10:55:59 +0530

    Added fontawesome Gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-emotion",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-fontawesome-css",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
